6c367c9dc2da4263724ff6e1b16d7a29e9e8d7ea
S Vineeth sajjan <vineethsajjan03@gmail.com>
2025-12-11 21:34:07 +0530

    Update program-1.java

--- program-1.java
+++ program-1.java
@@ -1,1 +1,45 @@
+class Calculator {
+    public double calculate(double a, double b, String operation) {
+        switch (operation.toLowerCase()) {
 
+            case "add":
+            case "+":
+                return a + b;
+
+            case "subtract":
+            case "-":
+                return a - b;
+
+            case "multiply":
+            case "*":
+                return a * b;
+
+            case "divide":
+            case "/":
+                if (b == 0) {
+                    System.out.println("Error: Division by zero!");
+                    return 0;
+                }
+                return a / b;
+
+            default:
+                System.out.println("Invalid operation type!");
+                return 0;
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        double a = 10.0;
+        double b = 5.0;
+
+        System.out.println("Addition: " + calc.calculate(a, b, "add"));
+        System.out.println("Subtraction: " + calc.calculate(a, b, "subtract"));
+        System.out.println("Multiplication: " + calc.calculate(a, b, "multiply"));
+        System.out.println("Division: " + calc.calculate(a, b, "divide"));
+    }
+}
