249633e03a4153dffb8b081bda11a282ac15aa12
S Vineeth sajjan <vineethsajjan03@gmail.com>
2025-12-11 21:44:59 +0530

    Implement odd number printer in program-2.java
    Added a program that prints the first 'a' odd numbers.

--- program-2.java
+++ program-2.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
 
+public class vineeth 
+{
+	  public static void main(String[] args)
+	  {
+	  Scanner scan= new Scanner(System.in);
+	  System.out.print("Enter a: ");
+      int a = scan.nextInt();
+      int num = 1;
+
+      for (int i = 1; i <= a; i++) {
+          System.out.print(num);
+          if (i < a) 
+          {
+              System.out.print(", ");
+          }
+          num += 2;
+	}
+}
+}
