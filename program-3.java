999a1a86e00f1c8005f606cbdd091616eeca16a9
S Vineeth sajjan <vineethsajjan03@gmail.com>
2025-12-12 08:50:37 +0530

    Implement odd number printing based on user input
    Added functionality to print odd numbers up to a user-defined limit.

--- program-3.java
+++ program-3.java
@@ -1,1 +1,27 @@
+package com.vini;
 
+import java.util.Scanner;
+
+public class vineeth 
+{
+	  public static void main(String[] args)
+	  {
+		  Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter a: ");
+	        int a = sc.nextInt();
+
+	        if (a % 2 == 0) 
+	        {
+	            a = a + 1;
+	        }
+	        for (int i = 1; i <= a; i += 2) 
+	        {
+	            System.out.print(i);
+	            if (i + 2 <= a) 
+	            {
+	                System.out.print(", ");
+	            }
+
+	}
+}
+}
